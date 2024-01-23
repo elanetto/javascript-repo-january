668234fcf33f2fee255fd00246014aaab9ca2420
elanetto <101054274+elanetto@users.noreply.github.com>
2024-01-23 14:38:27 +0100

    class 23rd of jan 2024

---
+++ week-4/tuesday-23rd/trim/js/find.js
@@ -0,0 +1,20 @@
+// Create the function find(arr, value),
+// which looks for the value in the array
+// arr and returns it's number,
+// if found, or -1 if not found
+
+[1,2,3]
+
+function find(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+
+}
+
+// test cases
+
+// ["hesh", "jonas", "liv", "jackie"]
+// [1, 2, 3, 18, 1000]
+
+// NOPE. TIRED. BYE.
